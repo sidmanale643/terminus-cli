e7492ad508de1395338c19d7998487ab0a4c92df
Sidhant <sidmanale643@gmail.com>
2025-10-30 18:39:18 +0530

    improve token counting logic.

--- models/llm.py
+++ models/llm.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List
+from typing import Optional, Any
 
 class ModelConfig(BaseModel):
     model_name : str
@@ -7,13 +7,17 @@ class ModelConfig(BaseModel):
 
 class Response(BaseModel):
     content: str
-    tool_calls: List[str]
-    model: str
-    temperature: float
-    prompt_tokens: int
-    response_tokens: int
+    tool_calls: Optional[Any] = None
+    stop_reason: Optional[str] = "end_turn"
+    reasoning: Optional[str] = None
+    model: Optional[str] = None
+    temperature: Optional[float] = None
+    prompt_tokens: Optional[int] = None
+    response_tokens: Optional[int] = None
 
     def count_total_tokens(self) -> int:
-        total_tokens = self.prompt_tokens + self.response_tokens
-        return total_tokens
+        if self.prompt_tokens and self.response_tokens:
+            total_tokens = self.prompt_tokens + self.response_tokens
+            return total_tokens
+        return 0
 
