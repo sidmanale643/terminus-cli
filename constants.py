49740f0cdbd3d1c66bf788f0b14c3ee29a5efef0
Sidhant <sidmanale643@gmail.com>
2025-10-30 18:38:54 +0530

    constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+DEFAULT_DATABASE_DIR = ".db/"
+
+DEFAULT_GEMINI_MODEL = "google/gemini-2.5-flash"
+DEFAULT_OPEN_ROUTER_MODEL = "z-ai/glm-4.6:exacto"
+DEFAULT_GROQ_MODEL = "moonshotai/kimi-k2-instruct-0905"
+
+DEFAULT_PROVIDER = "groq"
+DEFAULT_MODEL = "moonshotai/kimi-k2-instruct-0905"
