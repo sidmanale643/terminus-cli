aad5b77f86aa240efd7f89a97219797bfe98490c
Sidhant <sidmanale643@gmail.com>
2025-10-31 15:51:18 +0530

    update constants

--- constants.py
+++ constants.py
@@ -4,5 +4,5 @@ DEFAULT_GEMINI_MODEL = "google/gemini-2.5-flash"
 DEFAULT_OPEN_ROUTER_MODEL = "z-ai/glm-4.6:exacto"
 DEFAULT_GROQ_MODEL = "moonshotai/kimi-k2-instruct-0905"
 
-DEFAULT_PROVIDER = "groq"
-DEFAULT_MODEL = "moonshotai/kimi-k2-instruct-0905"
+DEFAULT_PROVIDER = "openrouter"
+DEFAULT_MODEL = "z-ai/glm-4.6:exacto"
